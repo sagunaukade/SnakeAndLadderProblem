d96898b55d3da0da1388ce2512f9efab7823d3e8
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-24 17:43:28 +0530

    Snake and Ladder game for start position 0

---
+++ SnakeLadderUc1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+	class SnakeLadderUc1
+	{
+		private static void Main(string[] args)
+		{
+			int position = 0;
+			while (position <= 100)
+			{
+				Console.WriteLine("Current Position of a player = " + position);
+				position++;
+			}
+		}
+	}
+}
+
