3275bf667d1bab1aed56c4895aab93de1dce672c
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-26 19:42:09 +0530

    Adding the number of times dice was played to win game and also position after every die role

---
+++ SnakeAndLadderUC6/SnakeAndLadderUC6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderUC6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Simulator program \n");
+            SnakeLadder player = new SnakeLadder();
+            player.PlayGame();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SnakeAndLadderUC6/SnakeAndLadderUC6/SnakeLadder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderUC6
+{
+    internal class SnakeLadder
+    {
+        public const int No_Play = 0, Ladder = 1, Snake = 2, Winning_Posi = 100;
+        public int player_Posi = 0, DiceRoll, DiceCount = 0;
+        public void PlayGame()
+        {
+            Console.WriteLine("\n start the game");
+            Console.WriteLine("Starting position player is: " + player_Posi);
+            Random rand = new Random();
+
+            while (player_Posi < Winning_Posi)
+            {
+                DiceRoll = rand.Next(1, 7);
+                DiceCount++;
+                Console.WriteLine("\n DiceCount {0} \n Player get Dice Number {1}", DiceCount, DiceRoll);
+
+                switch (rand.Next(0, 3))
+                {
+                    case No_Play:
+                        Console.WriteLine("\n No play Player Position: " + player_Posi);
+                        break;
+
+                    case Ladder:
+                        player_Posi += DiceRoll;
+                        if (player_Posi > Winning_Posi)
+                        {
+                            player_Posi -= DiceRoll;
+                        }
+                        Console.WriteLine("\n Ladder Player Position: " + player_Posi);
+                        break;
+
+                    case Snake:
+                        player_Posi -= DiceRoll;
+                        if (player_Posi < 0)
+                        {
+                            player_Posi = 0;
+                        }
+
+                        Console.WriteLine("\n Snake Player Position " + player_Posi);
+                        break;
+                }
+
+            }
+            Console.WriteLine("\n Congratulations you Won ");
+            Console.WriteLine("Number of times the Dice was played to win game: " + DiceCount);
+        }
+    }
+}
+
