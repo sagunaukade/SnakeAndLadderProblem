ffca635c252a4d41557b81ee9b3309b53152b494
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-26 19:36:23 +0530

    Adding Player reaches the winning position

---
+++ SnakeAndLadderUC4/SnakeAndLadderUC4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderUC4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Welcome to Snake and Ladder programs \n");
+            SnakeLadder player = new SnakeLadder();
+            player.PlaySnakeAndLadder();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SnakeAndLadderUC4/SnakeAndLadderUC4/SnakeLadder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderUC4
+{
+    internal class SnakeLadder
+    {
+        public const int No_Play = 0, Ladder = 1, Snake = 2, Winning_Posi = 100;
+        public int player_Posi = 0, DiceRoll;
+        public void PlaySnakeAndLadder()
+        {
+            Console.WriteLine("\n Start the Game ");
+            Console.WriteLine("\n Starting position player is: " + player_Posi);
+            Random random = new Random();
+
+            while (player_Posi < Winning_Posi)
+            {
+                DiceRoll = random.Next(1, 7);
+                Console.WriteLine("Player gets Dice Number: " + DiceRoll);
+
+                switch (random.Next(0, 3))
+                {
+                    case No_Play:
+                        Console.WriteLine("\n No Play");
+                        break;
+
+                    case Ladder:
+                        player_Posi += DiceRoll;
+                        Console.WriteLine("\n Ladder " + player_Posi);
+                        break;
+
+                    case Snake:
+                        player_Posi -= DiceRoll;
+                        Console.WriteLine("\n Snake " + player_Posi);
+                        if (player_Posi < 0)
+                        {
+                            player_Posi = 0;
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
